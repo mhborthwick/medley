e00dc87aa40fd65a78d1d9bd9ec5d55224aa947c
Mike Borthwick <mhborthwick@gmail.com>
2024-06-30 00:11:24 -0700

    setup workspace

--- authserver/cmd/main.go
+++ authserver/cmd/main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/google/uuid"
 )
+
+func GetRandomString() string {
+	return uuid.NewString()
+}
 
 func main() {
 	clientID := os.Getenv("CLIENT_ID")
+	randID := GetRandomString()
 	fmt.Println(clientID)
-	fmt.Println("hello, world")
+	fmt.Println(randID)
 }
